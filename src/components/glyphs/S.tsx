5bb5a2e63a5b64645a4f2bd2b017ee17b16e5eae
Riya Sinha <riya.v.sinha@gmail.com>
2024-04-30 22:38:12 -0700

    type glyphs

---
+++ src/components/glyphs/S.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const _path = `M92 26 A43 20 0 1 0 43 46 A42 23 0 1 1 9 68`;
+
+export const S = (props: React.SVGProps<SVGPathElement>) => (
+    <path fill="#ffffff" strokeWidth="18" {...props} d={_path} />
+);
---
+++ src/components/glyphs/glyph.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+type GlyphProps = {
+  /** the factor by which to scale the glyph's height. */
+  yscale: number;
+  /** if set, reflects the glyph vertically. */
+  inverted?: boolean;
+  /** the factor by which to scale the glyph's width. */
+  xscale: number;
+  /** the SVG contents to transform. */
+  children: React.ReactNode;
+};
+
+/**
+ * Container component which scales a square glyph to the required dimensions.
+ */
+const Glyph = ({ yscale, inverted, xscale, children }: GlyphProps) => {
+  const _yscale = yscale * (inverted ? -1 : 1);
+  const transform = `scale(${xscale}, ${_yscale})`;
+  return <g transform={transform}>{children}</g>;
+};
+export default Glyph;
