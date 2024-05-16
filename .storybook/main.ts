1a68b30ba77ebc4d7364dc66477b45820dec335d
Riya Sinha <riya.v.sinha@gmail.com>
2024-05-15 23:43:27 -0700

    try removing dts from storybook build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import { withoutVitePlugins } from "@storybook/builder-vite";
 
 const config: StorybookConfig = {
   stories: ["../**/*.mdx", "../**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -12,6 +13,12 @@ const config: StorybookConfig = {
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  async viteFinal(config) {
+    return {
+      ...config,
+      plugins: await withoutVitePlugins(config.plugins, ["vite:dts"]),
+    };
   },
 };
 export default config;
