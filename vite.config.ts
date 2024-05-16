203bb076e6edbc8c6e0cfc9100fa589af85ff1dc
Riya Sinha <riya.v.sinha@gmail.com>
2024-05-15 19:03:56 -0700

    add declaration file generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dts({ rollupTypes: true }), react()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
